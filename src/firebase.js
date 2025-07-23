1df8f6cbeccca55989914891acc1de08a7f3e3a7
Dipti Rathod <146316513+Dipti170@users.noreply.github.com>
2025-07-24 00:06:23 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,27 +1,4 @@
-// // firebase.js
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore";
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDtafmsOfryHFRC9z0CqJSpaeKD_KwBcz0",
-//   authDomain: "timberflow-170.firebaseapp.com",
-//   projectId: "timberflow-170",
-//   storageBucket: "timberflow-170.appspot.com",
-//   messagingSenderId: "394842183167",
-//   appId: "1:394842183167:web:26ad38b7a39ce45aaf83ca"
-// };
 
-// // ⚙️ Firebase App Init
-// const app = initializeApp(firebaseConfig);
-
-// // 🔒 Firebase Auth and Firestore Export
-// const auth = getAuth(app);
-// const db = getFirestore(app);
-
-// export { auth, db };
-// export default app;
-
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
@@ -37,5 +14,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-export { auth }; // ✅ Only export what you're using
+export { auth }; 
 
